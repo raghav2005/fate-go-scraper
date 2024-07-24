5b544cbbb475559c088b184784d6678eab3238b4
Raghav <raghavawasthi2005@gmail.com>
2024-07-25 03:45:05 +0530

    black formatting. TODO: modularize

--- retrieve_stuff.py
+++ retrieve_stuff.py
@@ -11,11 +11,12 @@ from bs4 import BeautifulSoup
 from loguru import logger
 from collections import defaultdict
 
+
 class WebRetriever:
-    def __init__(self, url, debug = False):
+    def __init__(self, url, debug=False):
         self.url = url
         self.debug = debug
-        
+
     def setup_selenium_driver(self):
         # setup chrome options for selenium
         chrome_options = Options()
@@ -24,10 +25,12 @@ class WebRetriever:
         chrome_options.add_argument("--disable-dev-shm-usage")
 
         # setup chrome driver with webdriver manager
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        self.driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=chrome_options
+        )
 
     # msg_type: one of ["success", "error", "trace", "info", "warning", "critical", "debug"]
-    def log(self, msg, msg_type = "success"):
+    def log(self, msg, msg_type="success"):
         if self.debug:
             # match msg_type:
             #     case "success":
@@ -53,8 +56,8 @@ class WebRetriever:
         else:
             if msg_type in ["error", "critical"]:
                 getattr(logger, msg_type)(msg)
-    
-    def load_webpage(self, url = None):
+
+    def load_webpage(self, url=None):
         if url is None:
             url = self.url
 
@@ -64,10 +67,11 @@ class WebRetriever:
         # load the webpage
         self.driver.get(url)
         self.log(f"requested webpage: {url}", "debug")
-        
+
     def quit_webdriver(self):
         # close the webdriver instance
         self.driver.quit()
+
 
 class FGOWebRetriever(WebRetriever):
     def __init__(self, url, debug=False):
@@ -77,7 +81,7 @@ class FGOWebRetriever(WebRetriever):
     def get_QAB_from_HTML_class(self, list_of_HTML_classes):
         for HTML_class in list_of_HTML_classes:
             if "npinfobox" in HTML_class:
-                return HTML_class.split('-')[1][0].upper()
+                return HTML_class.split("-")[1][0].upper()
 
         self.log("Unknown NP QAB", "warning")
         return "Unknown"
@@ -99,22 +103,36 @@ class FGOWebRetriever(WebRetriever):
             chars_in_tier = defaultdict(list)
 
             for tier_table in tier_tables:
-                tier = tier_table.find("tr").find_all('th')[0].get_text()
+                tier = tier_table.find("tr").find_all("th")[0].get_text()
                 self.log(f"tier: {tier}", "info")
 
                 for tier_entry in tier_table.find("td"):
                     try:
-                        char_name = tier_entry.find("span", {"class": "tier-servant-name-span"}).get_text()
+                        char_name = tier_entry.find(
+                            "span", {"class": "tier-servant-name-span"}
+                        ).get_text()
 
-                        char_tier_np_info = tier_entry.find("div", {"class": "FGOTierNPInfo"})
-                        char_rarity = len(char_tier_np_info.find("span", {"class": "star-rarity"}).get_text())
+                        char_tier_np_info = tier_entry.find(
+                            "div", {"class": "FGOTierNPInfo"}
+                        )
+                        char_rarity = len(
+                            char_tier_np_info.find(
+                                "span", {"class": "star-rarity"}
+                            ).get_text()
+                        )
                         char_type = char_tier_np_info.find("div").get_text()
-                        char_NP_QAB = self.get_QAB_from_HTML_class(char_tier_np_info.get("class"))
+                        char_NP_QAB = self.get_QAB_from_HTML_class(
+                            char_tier_np_info.get("class")
+                        )
 
-                        char_summary = tier_entry.find("div", {"class": "tier-expl-container"}).get_text()
+                        char_summary = tier_entry.find(
+                            "div", {"class": "tier-expl-container"}
+                        ).get_text()
 
-                        char_urls = tier_entry.find("div", {"class": "FGOTierServantIcon"})
-                        char_page_url = char_urls.find("a").get("href").split('/')[-1]
+                        char_urls = tier_entry.find(
+                            "div", {"class": "FGOTierServantIcon"}
+                        )
+                        char_page_url = char_urls.find("a").get("href").split("/")[-1]
                         char_img_url = char_urls.find("img").get("src")
 
                         char_info = {
@@ -124,7 +142,7 @@ class FGOWebRetriever(WebRetriever):
                             "type": char_type,
                             "NP_QAB": char_NP_QAB,
                             "img_url": char_img_url,
-                            "servant_page_partial_url": char_page_url
+                            "servant_page_partial_url": char_page_url,
                         }
                         chars_in_tier[tier].append(char_info)
                         self.log(char_info, "debug")
@@ -137,11 +155,11 @@ class FGOWebRetriever(WebRetriever):
 
         except Exception as e:
             self.log(f"Error retrieving tier list info: {e}", "error")
-            self.quit_webdriver()            
+            self.quit_webdriver()
             return defaultdict(list)
 
 
 if __name__ == "__main__":
     url = "https://fgo.gamepress.gg/servant-tier-list"
-    fgo_scraper = FGOWebRetriever(url, debug = True)
+    fgo_scraper = FGOWebRetriever(url, debug=True)
     # fgo_scraper.log(fgo_scraper.chars_in_tier, "debug")
