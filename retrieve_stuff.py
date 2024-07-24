a0e2669eb498fc4401a996648a94fa71c1623307
Raghav <raghavawasthi2005@gmail.com>
2024-07-25 03:44:49 +0530

    put everything as classes

--- retrieve_stuff.py
+++ retrieve_stuff.py
@@ -11,76 +11,137 @@ from bs4 import BeautifulSoup
 from loguru import logger
 from collections import defaultdict
 
-# setup chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # ensure GUI is off
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+class WebRetriever:
+    def __init__(self, url, debug = False):
+        self.url = url
+        self.debug = debug
+        
+    def setup_selenium_driver(self):
+        # setup chrome options for selenium
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # ensure GUI is off
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
 
-# setup chrome driver with webdriver manager
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-# url of the webpage to scrape
-url = "https://fgo.gamepress.gg/servant-tier-list"
+        # setup chrome driver with webdriver manager
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-# load the webpage
-driver.get(url)
-logger.success(f"requested webpage: {url}")
+    # msg_type: one of ["success", "error", "trace", "info", "warning", "critical", "debug"]
+    def log(self, msg, msg_type = "success"):
+        if self.debug:
+            # match msg_type:
+            #     case "success":
+            #         logger.success(msg)
+            #     case "error":
+            #         logger.error(msg)
+            #     case "trace":
+            #         logger.trace(msg)
+            #     case "info":
+            #         logger.info(msg)
+            #     case "warning":
+            #         logger.warning(msg)
+            #     case "critical":
+            #         logger.critical(msg)
+            #     case default:
+            #         logger.debug(msg)
 
-def get_QAB_from_HTML_class(list_of_HTML_classes):
-    for HTML_class in list_of_HTML_classes:
-        if "npinfobox" in HTML_class:
-            return HTML_class.split('-')[1][0].upper()
+            try:
+                getattr(logger, msg_type)(msg)
+            except Exception as e:
+                logger.error(e)
 
-    return "Unknown"
+        else:
+            if msg_type in ["error", "critical"]:
+                getattr(logger, msg_type)(msg)
+    
+    def load_webpage(self, url = None):
+        if url is None:
+            url = self.url
 
-try:
-    # wait until the desired element is present
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "view-content"))
-    )
-    logger.success("webpage loaded")
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    logger.success("parsed webpage")
+        # setup drivers
+        self.setup_selenium_driver()
 
-except Exception as e:
-    logger.error(e)
+        # load the webpage
+        self.driver.get(url)
+        self.log(f"requested webpage: {url}", "debug")
+        
+    def quit_webdriver(self):
+        # close the webdriver instance
+        self.driver.quit()
 
-tier_tables = soup.find_all("table", {"class": "fgo-tier-table"})
-chars_in_tier = defaultdict(list)
+class FGOWebRetriever(WebRetriever):
+    def __init__(self, url, debug=False):
+        super().__init__(url, debug)
+        self.chars_in_tier = self.get_tier_list_info()
 
-for tier_table in tier_tables:
-    tier = tier_table.find("tr").find_all('th')[0].get_text()
-    print(f"tier: {tier}")
+    def get_QAB_from_HTML_class(self, list_of_HTML_classes):
+        for HTML_class in list_of_HTML_classes:
+            if "npinfobox" in HTML_class:
+                return HTML_class.split('-')[1][0].upper()
 
-    try:
-        for tier_entry in tier_table.find("td"):
-            char_name = tier_entry.find("span", {"class": "tier-servant-name-span"}).get_text()
+        self.log("Unknown NP QAB", "warning")
+        return "Unknown"
 
-            char_tier_np_info = tier_entry.find("div", {"class": "FGOTierNPInfo"})
-            char_rarity = len(char_tier_np_info.find("span", {"class": "star-rarity"}).get_text())
-            char_type = char_tier_np_info.find("div").get_text()
-            char_NP_QAB = get_QAB_from_HTML_class(char_tier_np_info.get("class"))
+    def get_tier_list_info(self):
+        self.load_webpage()
 
-            char_summary = tier_entry.find("div", {"class": "tier-expl-container"}).get_text()
+        try:
+            # wait until content is present
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "view-content"))
+            )
 
-            char_urls = tier_entry.find("div", {"class": "FGOTierServantIcon"})
-            char_page_url = char_urls.find("a").get("href").split('/')[-1]
-            char_img_url = char_urls.find("img").get("src")
+            self.log("webpage loaded")
+            soup = BeautifulSoup(self.driver.page_source, "html.parser")
+            self.log("parsed webpage", "debug")
 
-            chars_in_tier[tier].append({
-                "name": char_name,
-                "rarity": char_rarity,
-                "summary": char_summary,
-                "type": char_type,
-                "NP_QAB": char_NP_QAB,
-                "img_url": char_img_url,
-                "servant_page_partial_url": char_page_url
-            })
+            tier_tables = soup.find_all("table", {"class": "fgo-tier-table"})
+            chars_in_tier = defaultdict(list)
 
-    except Exception as e:
-        logger.error(e)
+            for tier_table in tier_tables:
+                tier = tier_table.find("tr").find_all('th')[0].get_text()
+                self.log(f"tier: {tier}", "info")
 
-print(chars_in_tier)
+                for tier_entry in tier_table.find("td"):
+                    try:
+                        char_name = tier_entry.find("span", {"class": "tier-servant-name-span"}).get_text()
 
-# close the webdriver instance
-driver.quit()
+                        char_tier_np_info = tier_entry.find("div", {"class": "FGOTierNPInfo"})
+                        char_rarity = len(char_tier_np_info.find("span", {"class": "star-rarity"}).get_text())
+                        char_type = char_tier_np_info.find("div").get_text()
+                        char_NP_QAB = self.get_QAB_from_HTML_class(char_tier_np_info.get("class"))
+
+                        char_summary = tier_entry.find("div", {"class": "tier-expl-container"}).get_text()
+
+                        char_urls = tier_entry.find("div", {"class": "FGOTierServantIcon"})
+                        char_page_url = char_urls.find("a").get("href").split('/')[-1]
+                        char_img_url = char_urls.find("img").get("src")
+
+                        char_info = {
+                            "name": char_name,
+                            "rarity": char_rarity,
+                            "summary": char_summary,
+                            "type": char_type,
+                            "NP_QAB": char_NP_QAB,
+                            "img_url": char_img_url,
+                            "servant_page_partial_url": char_page_url
+                        }
+                        chars_in_tier[tier].append(char_info)
+                        self.log(char_info, "debug")
+
+                    except Exception as e:
+                        self.log(f"Error processing entry: {e}", "error")
+
+            self.quit_webdriver()
+            return chars_in_tier
+
+        except Exception as e:
+            self.log(f"Error retrieving tier list info: {e}", "error")
+            self.quit_webdriver()            
+            return defaultdict(list)
+
+
+if __name__ == "__main__":
+    url = "https://fgo.gamepress.gg/servant-tier-list"
+    fgo_scraper = FGOWebRetriever(url, debug = True)
+    # fgo_scraper.log(fgo_scraper.chars_in_tier, "debug")
